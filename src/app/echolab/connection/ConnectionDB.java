3429964da9ea9f5259f40bd73c218d7598e4b8a1
MarioGonzalez99 <39312530+MarioGonzalez99@users.noreply.github.com>
2020-06-29 21:04:15 -0600

    Agregando decriptacion de datos a la conexion

--- src/app/echolab/connection/ConnectionDB.java
+++ src/app/echolab/connection/ConnectionDB.java
@@ -5,18 +5,33 @@
  */
 package app.echolab.connection;
 
+import app.echolab.properties.ConfigProperties;
+import app.echolab.utilities.Decrypt;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Mario
  */
 public class ConnectionDB {
     
-    public static Connection openConnection(String Url, String User, String Password, String Driver) throws SQLException, ClassNotFoundException{
-        Class.forName(Driver);
-        return DriverManager.getConnection(Url, User, Password);
+    public static Connection openConnection(String User, String Password) throws SQLException, ClassNotFoundException{
+        Properties dbProps = new Properties();
+        try(InputStream propertyFile = ConfigProperties.getResourceAsInputStream("dbconfig.properties")){
+            dbProps.load(propertyFile);
+        } catch (IOException ex) {
+            Logger.getLogger(ConnectionDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String url = Decrypt.decryptText(dbProps.getProperty("url"));
+        String driver = Decrypt.decryptText(dbProps.getProperty("driver"));
+        Class.forName(driver);
+        return DriverManager.getConnection(url, User, Password);
     }
     
     public static void closeConnection(Connection con) throws SQLException{
