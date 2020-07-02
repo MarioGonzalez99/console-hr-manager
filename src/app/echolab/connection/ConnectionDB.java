9800b0c0251e7a5685a3ba8fdc94f3eab01354d0
MarioGonzalez99 <39312530+MarioGonzalez99@users.noreply.github.com>
2020-07-02 09:15:09 -0600

    Integracion de pool de conexiones a la clase ConnectionDB

--- src/app/echolab/connection/ConnectionDB.java
+++ src/app/echolab/connection/ConnectionDB.java
@@ -15,23 +15,45 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.LoggerFactory;
 /**
  *
  * @author Mario
  */
 public class ConnectionDB {
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ConnectionDB.class);
+    private static final ConnectionDB main = new ConnectionDB();
+    private static BasicDataSource basicDataSource;
     
-    public static Connection openConnection(String User, String Password) throws SQLException, ClassNotFoundException{
+    private ConnectionDB(){
+    }
+    
+    public static void loadConnection(String User, String Password){
         Properties dbProps = new Properties();
-        try(InputStream propertyFile = ConfigProperties.getResourceAsInputStream("dbconfig.properties")){
+        try(InputStream propertyFile = ConfigProperties.getResourceAsInputStream("dbConfig.properties")){
             dbProps.load(propertyFile);
         } catch (IOException ex) {
             Logger.getLogger(ConnectionDB.class.getName()).log(Level.SEVERE, null, ex);
         }
         String url = Decrypt.decryptText(dbProps.getProperty("url"));
         String driver = Decrypt.decryptText(dbProps.getProperty("driver"));
-        Class.forName(driver);
-        return DriverManager.getConnection(url, User, Password);
+        basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(driver);
+        basicDataSource.setUsername(User);
+        basicDataSource.setPassword(Password);
+        basicDataSource.setUrl(url);
+        basicDataSource.setInitialSize(4); //The initial number of connections that are created when the pool is started. 
+        basicDataSource.setMaxTotal(10); //The maximum number of active connections that can be allocated from this pool at the same time, or negative for no limit.
+        basicDataSource.setMaxIdle(8);
+    }
+    
+    public static ConnectionDB getInstance(){
+        return main;
+    }
+    
+    public static Connection openConnection() throws SQLException, ClassNotFoundException{
+        return basicDataSource.getConnection();
     }
     
     public static void closeConnection(Connection con) throws SQLException{
